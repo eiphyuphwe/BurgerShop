3a518ccd5f346d5ff8f4653d98a5c809518b19b6
eiphyuphwe <eiphyuphwe643@gmail.com>
2025-03-26 01:24:55 +0800

    added code quality check

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
+    id("io.gitlab.arturbosch.detekt") version "1.23.1"
+    id("org.jetbrains.dokka") version "1.9.10"
 }
 
 android {
